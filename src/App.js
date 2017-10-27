cfd2ad22d7cd7b23c4cf55d829d21652cb10fc22
peagle <pyy7891@g.rit.edu>
2017-10-26 21:15:39 -0400

    - Added state to list component - Added search and delete mechanism to list component - Added event handlers and used React synthetic event to get a form element value.

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ const list = [
         objectID: 0,
     },
     {
-        title: 'Redux',
-        url: 'https://github.com/reactjs/redux',
+        title: 'Vuejs',
+        url: 'https://vuejs.com',
         author: 'Dan Abramov, Andrew Clark',
         num_comments: 2,
         points: 5,
@@ -19,20 +19,58 @@ const list = [
     },
 ];
 
+const isSearched = (searchTerm) => (item) => item.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase());
+
+
 class App extends Component {
-  render() {
-        const listItems = list.map((item) => {
-            return <li key={item.objectID}>{item.title}</li>
+
+    constructor(){
+        super();
+
+        this.state = {
+            list: list,
+            searchTerm: ''
+        }
+
+        this.onDelete = this.onDelete.bind(this);
+        this.onSearch = this.onSearch.bind(this);
+    }
+
+    onDelete(itemKey){
+        const updatedList = this.state.list.filter((item) => item.objectID !== itemKey);
+        this.setState({list: updatedList});
+    }
+
+    onSearch(event){
+        this.setState({
+            searchTerm: event.target.value
+        });
+    }
+
+
+
+    render() {
+        const listItems = this.state.list.filter(isSearched(this.state.searchTerm)).map((item) => {
+            return <li key={item.objectID}>
+                    <div>{item.title} &nbsp; &nbsp;
+                        <span><button onClick={() => this.onDelete(item.objectID)} >X</button></span>
+                    </div>
+                </li>
         });
 
         return (
             <div>
+                <div>
+                    <form>
+                        <input type="text" name="search" onChange={this.onSearch}></input>
+                    </form>
+                </div>
                 <ul>
                  {listItems}
                 </ul>
             </div>
         );
-  }
+    }
 }
 
 export default App;
