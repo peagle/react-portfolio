3a7968a23891a28ec15e2ab1736f721e041adf95
peagle <pyy7891@g.rit.edu>
2017-11-11 09:03:25 -0500

    - Added 'ref' to reference a DOM element <input> and gave it focus.

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,28 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Button from '../button';
 
-function Search (props) {
-    const{onSubmit, onChange, value, onNextPage, children} = props;
+class Search extends Component {
+    constructor(props){
+        super(props);
+        this.state = props;
+    }
 
-    return (
-        <div>
-            <form onSubmit={onSubmit}>
-                {children}: &nbsp;
-                <input type="text"
-                       name="search"
-                       onChange={onChange}
-                       value={value}
-                />
-                <button type="submit">
-                    {children}
-                </button>
-                <Button onClick={onNextPage}>
-                    Next Page
-                </Button>
-            </form>
-        </div>
-    );
+    render(){
+        const{onSubmit, onChange, value, onNextPage, children} = this.state;
+        let input;
+        return (
+            <div>
+                <form onSubmit={onSubmit}>
+                    {children}: &nbsp;
+                    <input type="text"
+                           name="search"
+                           onChange={onChange}
+                           value={value}
+                           ref={inputRef => this.input = inputRef}
+                    />
+                    <button type="submit">
+                        {children}
+                    </button>
+                    <Button onClick={onNextPage}>
+                        Next Page
+                    </Button>
+                </form>
+            </div>
+        );
+    }
+
+    componentDidMount(){
+        this.input.focus();
+    }
 }
 
 export default Search;
